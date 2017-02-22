1aa3ceca73c180af6f48a2bb9a4ab092a52cfa55
joonaleppalahti <joona.leppalahti@myy.haaga-helia.fi>
2017-02-22 15:18:41 +0200

    updated script

--- capture.py
+++ capture.py
@@ -4,14 +4,20 @@ import cv2
 import os
 
 camera = cv2.VideoCapture(0)
-camera.set(cv2.cv.CV_CAP_PROP_FPS, 1)
+#camera.set(cv2.cv.CV_CAP_PROP_FPS, 1)
 time.sleep(1.0)
 background = None
 frameNumber = 0
 frameCount = 0
 imgDir = "img"
 start = time.time()
-thresholdLimit = 20
+minArea = 550
+thresholdLimit = 80
+dilationPixels = 10
+gaussianPixels = 31
+wroteImg = 0
+writeTime = 0
+
 
 while(True):
 	#capture frame by frame
@@ -19,17 +25,26 @@ while(True):
 
 	#operations on the frame
 	currentFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	currentFrame = cv2.GaussianBlur(currentFrame, (gaussianPixels, gaussianPixels), 0)
 
 	if background is None:
 		background = currentFrame
 		continue
 
-	currentFrame = cv2.GaussianBlur(currentFrame, (31, 31), 0)
+	#reset background frame, y u no work??
+	"""if cv2.waitKey(1) & 0xFF == ord("r"):
+		time.sleep(1.0)
+		background = None
+		ret, frame = camera.read()
+		currentFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		currentFrame = cv2.GaussianBlur(currentFrame, (gaussianPixels, gaussianPixels), 0)
+		background = currentFrame
+		continue"""
 
 	#compare frames
 	frameDelta = cv2.absdiff(background, currentFrame)
 	thresh = cv2.threshold(frameDelta, thresholdLimit, 255, cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations=30)
+	thresh = cv2.dilate(thresh, None, iterations=dilationPixels)
 	(contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #find contours
 
 	#fps
@@ -37,6 +52,9 @@ while(True):
 	seconds = end - start
 	fps = round((1 / seconds), 1)
 	start = time.time()
+
+	secondInt = int(time.time())
+
 	cv2.putText(frame, "fps: {}".format(fps), (400, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 140, 255), 1)
 
 	for contour in contours:
@@ -50,14 +68,20 @@ while(True):
 		cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 		frameCount += 1
 
-		if frameCount == 24:
-			#save images when movement is detected
+		#save images when movement is detected
+		if wroteImg == 0:
 			cv2.imwrite(os.path.join(imgDir, str(frameNumber))+".png", frame)
 			frameNumber += 1
 			frameCount = 0
+			wroteImg = 1
+			writeTime = secondInt
 
+		if secondInt > writeTime:
+			wroteImg = 0
 	#display result
-	cv2.imshow("Feed",frame)
+	cv2.imshow("feed",frame)
+#	cv2.imshow("delta",frameDelta)
+#	cv2.imshow("Thresh",thresh)
 
 	if cv2.waitKey(1) & 0xFF == ord("q"):
 		break
